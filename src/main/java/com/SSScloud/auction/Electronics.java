36bd6b0f8af2dd0d904c5a2c22d2800ee144d215
Kphong-debug <kphongtq12082007@gmail.com>
2026-03-29 16:57:54 +0700

    Class cơ bản

--- src/main/java/com/SSScloud/auction/Electronics.java
+++ src/main/java/com/SSScloud/auction/Electronics.java
@@ -1,5 +1,13 @@
 package com.SSScloud.auction;
 
+import java.time.LocalDate;
+
 public class Electronics extends Item{
-    
+    private int WarantineTime;
+
+    public Electronics(String Id, String Name, double BasePrice, LocalDate Manufacturingime, String Author, String ItemDescibe, int WarantineTime){
+        super(Id, Name, BasePrice, Manufacturingime, Author, ItemDescibe);
+        this.WarantineTime = WarantineTime;
+    }
+
 }
--- src/main/java/com/SSScloud/auction/Item.java
+++ src/main/java/com/SSScloud/auction/Item.java
@@ -5,15 +5,31 @@ import java.time.LocalDate;
 public abstract class Item extends Entity{
     private double BasePrice;
     private LocalDate Manufacturingime;
+    private String Author, ItemDescibe;
 
-    public Item(String Id, String Name, double BasePrice, LocalDate Manufacturingime){
+    public Item(String Id, String Name, double BasePrice, LocalDate Manufacturingime, String Author, String ItemDescibe){
         super(Id, Name);
-        this.BasePrice;= BasePrice;
+        this.BasePrice = BasePrice;
         this.Manufacturingime = Manufacturingime;
+        this.Author = Author;
+        this.ItemDescibe = ItemDescibe;
     }
 
     public void SetBasePrice(double BasePrice){
         this.BasePrice = BasePrice;
+    }
+
+    public double getFinalPrice(){
+        return this.BasePrice;
+    }
+
+    public String getAuthor(){
+        return this.Author;        
+    }
+    
+    @Override
+    public String toString() {
+        return this.ItemDescibe;
     }
 }
 
