4feef247878484ff7cf6ee6a1897560aa96132f6
tuetistoof <tweantiit@gmail.com>
2026-03-30 01:52:34 +0700

    fix some bug

--- src/main/java/com/SSScloud/auction/Bidder.java
+++ src/main/java/com/SSScloud/auction/Bidder.java
@@ -2,7 +2,7 @@ package com.SSScloud.auction;
 
 public class Bidder extends User {
     private long AccountBalance;
-    public Bidder (String Id, String Name, String UserName, String Password, String AccountBalance)
+    public Bidder (String Id, String Name, String UserName, String Password, long AccountBalance)
     {
         super (Id, Name, UserName, Password);
         this.AccountBalance = AccountBalance;
