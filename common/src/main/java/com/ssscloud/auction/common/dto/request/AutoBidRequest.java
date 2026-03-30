87e1401d148a3f1d4ed38099168dad95ba4aa5f3
Hoàng Hà Anh <intotheunknownohhh@gmail.com>
2026-03-30 22:53:33 +0700

    DTO request

--- common/src/main/java/com/ssscloud/auction/common/dto/request/AutoBidRequest.java
+++ common/src/main/java/com/ssscloud/auction/common/dto/request/AutoBidRequest.java
@@ -1,5 +1,35 @@
 package com.ssscloud.auction.common.dto.request;
 
-public class AutoBidRequest {
+import java.io.Serializable;
+
+public class AutoBidRequest implements Serializable{
+    //serialVersion dùng để ghi nhớ các phiên bản của Serializable
+    private static final long serialVersionUID = 1L;
+
+    private Long auctionId;
+    private double maxBid;
+    private double increment;
+
+    public AutoBidRequest() {}
+
+    public Long getAuctionId() { return auctionId; }
+    public void setAuctionId(Long auctionId) { this.auctionId = auctionId; }
+
+    public double getMaxBid() { return maxBid; }
+    public void setMaxBid(double maxBid) { this.maxBid = maxBid; }
+
+    public double getIncrement() { return increment; }
+    public void setIncrement(double increment) { this.increment = increment; }
     
+    @Override
+    public String toString() {
+        return "AutoBidRequest{" +
+                "auctionId=" + auctionId +
+                ", maxBid=" + maxBid +
+                ", increment=" + increment +
+                '}';
+    }
 }
+
+//Cấu trúc 1 DTO: serialVersion, thuộc tính truyền đi, getter/setter, constructor mặc định và tham số
+// toString để giúp debug
