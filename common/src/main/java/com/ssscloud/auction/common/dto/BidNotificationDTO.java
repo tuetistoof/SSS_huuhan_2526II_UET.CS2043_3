aa890a8470a5fb60c7f6cf09a0b4191540d35a1d
Hoàng Hà Anh <intotheunknownohhh@gmail.com>
2026-03-30 23:38:05 +0700

    dto + enum

--- common/src/main/java/com/ssscloud/auction/common/dto/BidNotificationDTO.java
+++ common/src/main/java/com/ssscloud/auction/common/dto/BidNotificationDTO.java
@@ -1,5 +1,41 @@
 package com.ssscloud.auction.common.dto;
 
-public class BidNotificationDTO {
-    
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class BidNotificationDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long auctionId;
+    private double newPrice;
+    private String highestBidder;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public BidNotificationDTO() {}
+
+    public Long getAuctionId() { return auctionId; }
+    public void setAuctionId(Long auctionId) { this.auctionId = auctionId; }
+
+    public double getNewPrice() { return newPrice; }
+    public void setNewPrice(double newPrice) { this.newPrice = newPrice; }
+
+    public String getHighestBidder() { return highestBidder; }
+    public void setHighestBidder(String highestBidder) { this.highestBidder = highestBidder; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+
+    @Override
+    public String toString() {
+        return "BidNotificationDTO{" +
+                "auctionId=" + auctionId +
+                ", newPrice=" + newPrice +
+                ", highestBidder='" + highestBidder + '\'' +
+                '}';
+    }
 }
--- common/src/main/java/com/ssscloud/auction/common/dto/response/BidDTO.java
+++ common/src/main/java/com/ssscloud/auction/common/dto/response/BidDTO.java
@@ -1,5 +1,41 @@
 package com.ssscloud.auction.common.dto.response;
 
-public class BidDTO {
-    
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class BidDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private Long auctionId;
+    private String bidderUsername;
+    private double bidAmount;
+    private LocalDateTime bidTime;
+
+    public BidDTO() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getAuctionId() { return auctionId; }
+    public void setAuctionId(Long auctionId) { this.auctionId = auctionId; }
+
+    public String getBidderUsername() { return bidderUsername; }
+    public void setBidderUsername(String bidderUsername) { this.bidderUsername = bidderUsername; }
+
+    public double getBidAmount() { return bidAmount; }
+    public void setBidAmount(double bidAmount) { this.bidAmount = bidAmount; }
+
+    public LocalDateTime getBidTime() { return bidTime; }
+    public void setBidTime(LocalDateTime bidTime) { this.bidTime = bidTime; }
+
+    @Override
+    public String toString() {
+        return "BidDTO{" +
+                "id=" + id +
+                ", bidderUsername='" + bidderUsername + '\'' +
+                ", bidAmount=" + bidAmount +
+                ", bidTime=" + bidTime +
+                '}';
+    }
 }
