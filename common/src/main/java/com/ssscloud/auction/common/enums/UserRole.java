b8dd9b125d3f36f77cf96c4b7795f2848f337a2b
tuetistoof <tweantiit@gmail.com>
2026-03-30 15:38:54 +0700

    build abstract class

--- common/src/main/java/com/ssscloud/auction/common/enums/UserRole.java
+++ common/src/main/java/com/ssscloud/auction/common/enums/UserRole.java
@@ -1,5 +1,17 @@
 package com.ssscloud.auction.common.enums;
 
-public class UserRole {
+public enum UserRole {
+    BIDDER,
+    SELLER,
+    ADMIN;
     
+    public boolean isBidder(){
+        return this == BIDDER;
+    }
+    public boolean isSeller(){
+        return this == SELLER;
+    }
+    public boolean isAdmin(){
+        return this == ADMIN;
+    }
 }
--- common/src/main/java/com/ssscloud/auction/common/model/base/Entity.java
+++ common/src/main/java/com/ssscloud/auction/common/model/base/Entity.java
@@ -1,18 +1,38 @@
 package com.ssscloud.auction.common.model.base;
 
-public abstract class Entity {
-    private String Name, Id;
+import java.time.LocalDate;
 
-    public Entity(String Id, String Name){
-        this.Name = Name;
+public abstract class Entity {
+    private String Id;
+    private String Name;
+    private LocalDate  ManufacturingDate;
+    public Entity(String Id, String Name, LocalDate ManufacturingDate){
         this.Id = Id;
+        this.Name = Name;
+        this.ManufacturingDate = ManufacturingDate;
+    }
+
+    public String getName(String Name){
+        return this.Name;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public LocalDate getManufacturingDate() {
+        return ManufacturingDate;
     }
 
     public void setName(){
         this.Name = Name;   
     }
 
-    public String getName(String Name){
-        return this.Name;
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public void setManufacturingDate(LocalDate manufacturingDate) {
+        ManufacturingDate = manufacturingDate;
     }
 }
