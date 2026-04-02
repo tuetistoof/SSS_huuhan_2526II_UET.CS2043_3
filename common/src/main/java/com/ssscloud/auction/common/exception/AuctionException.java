c97b389b5071ae00fa01f08af45e2f485d0f1f91
Hoàng Hà Anh <intotheunknownohhh@gmail.com>
2026-04-02 23:37:40 +0700

    Exception cơ bản

--- common/src/main/java/com/ssscloud/auction/common/exception/AuctionException.java
+++ common/src/main/java/com/ssscloud/auction/common/exception/AuctionException.java
@@ -1,5 +1,18 @@
 package com.ssscloud.auction.common.exception;
 
-public class AuctionException {
+public class AuctionException extends RuntimeException {
+    private final String errorCode; //để gọi tên 1 số lỗi như BID_TOO_LOW,...
+
+    public AuctionException(String errorCode, String message){
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode(){
+        return this.errorCode;
+    }
+
+
+
     
 }
--- common/src/main/java/com/ssscloud/auction/common/exception/InvalidBidException.java
+++ common/src/main/java/com/ssscloud/auction/common/exception/InvalidBidException.java
@@ -1,5 +1,12 @@
 package com.ssscloud.auction.common.exception;
 
-public class InvalidBidException {
+public class InvalidBidException extends AuctionException {
+    public InvalidBidException(String message){
+        super("INVALID_BID", message);
     
+    }
+    public InvalidBidException(){
+        super("INVALID_BID", "Your bid is invalid, try again");
+    }
+
 }
