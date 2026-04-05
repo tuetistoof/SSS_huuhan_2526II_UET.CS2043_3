6aeade909f84368fc96cdae3e2ac6678a6567bb4
Kphong-debug <kphongtq12082007@gmail.com>
2026-04-05 11:16:58 +0700

    Tạo server

--- server/src/main/java/com/ssscloud/auction/server/networking/ClientHandler.java
+++ server/src/main/java/com/ssscloud/auction/server/networking/ClientHandler.java
@@ -1,5 +1,37 @@
 package com.ssscloud.auction.server.networking;
 
-public class ClientHandler {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+    private Socket clientSocket;
+
+    public ClientHandler(Socket socket){
+        this.clientSocket = socket;
+    }
+    @Override
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(),"UTF-8"), true);
+
+            MessageHandler messageHandler = new MessageHandler();
+
+            String jsonFromClient;
+            while((jsonFromClient = reader.readLine()) != null){
+                String jsonReponse = messageHandler.proccessRequest(jsonFromClient);
+                writer.print(jsonReponse);
+            }
+        } catch (IOException e) {
+            System.out.println("Client đã ngắt kết nối");
+        } finally{
+            this.clientSocket.close();
+        }
+
+    }
 }
--- server/src/main/java/com/ssscloud/auction/server/networking/MessageHandler.java
+++ server/src/main/java/com/ssscloud/auction/server/networking/MessageHandler.java
@@ -1,5 +1,45 @@
 package com.ssscloud.auction.server.networking;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import netscape.javascript.JSObject;
+
 public class MessageHandler {
-    
+    private  Gson gson = new Gson();
+    private BidController bidController = new BidController();
+    private UserController userController = new UserController();
+
+    public String proccessRequest(String jsonFromClient){
+        try {
+            JsonObject requestClient = JsonParser.parseString(jsonFromClient).getAsJsonObject();
+
+            String action = requestClient.get("action").getAsString();
+            String payload = requestClient.getAsJsonObject("payload");
+
+            switch (action) {
+                case "PLACE_BID":
+                    BidDTO bidDTO = gson.tojson(payload, BidDTO.class);
+                    return bidController.handle(bidDTO);
+                case "LOGIN":
+                    UserDTO userDTO = gson.tojson(payload, UserDTO.class);
+                    return userController.handle(userDTO);
+                case "GET_AUTION_LIST":
+                    return bidController.getAuctionList();
+                default:
+                    return createErrorReponse("Hành động không hợp lệ");
+            }   
+        } catch (Exception e) {
+            e.printStackTrace();
+            return createErrorReponse("Lỗi cú pháp Json hoặc lỗi hệ thống");
+        }
+    }
+
+    private createErrorReponse(String errorMessage){
+            JsonObject errorReponse = new JSObject();
+            errorReponse.addProperty("status", "Error");
+            errorMessage.addProperty("message", errorMessage);
+            return errorMessage.toString();
+    }
 }
