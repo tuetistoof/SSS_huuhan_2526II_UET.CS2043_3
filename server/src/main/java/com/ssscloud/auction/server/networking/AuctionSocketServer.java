30488dbd4e7c682964a7925199e59efc168457c9
Kphong-debug <kphongtq12082007@gmail.com>
2026-04-05 11:16:58 +0700

    Tạo server

--- server/src/main/java/com/ssscloud/auction/server/networking/AuctionSocketServer.java
+++ server/src/main/java/com/ssscloud/auction/server/networking/AuctionSocketServer.java
@@ -1,6 +1,24 @@
 package com.ssscloud.auction.server.networking;
 
-public 
- {
-    
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class AuctionSocketServer{
+    private static final int maxThread = 1000;
+    private static ExecutorService pool = Executors.newCachedThreadPool();
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+
+                pool.execute(new ClientHandler(clientSocket));
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
