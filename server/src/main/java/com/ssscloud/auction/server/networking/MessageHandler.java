474c3a546aa69ae8eca12a869aeae0299c280a22
Hoàng Hà Anh <intotheunknownohhh@gmail.com>
2026-04-07 17:55:01 +0700

    fix messageHandler + jsonUtil

--- server/src/main/java/com/ssscloud/auction/server/networking/MessageHandler.java
+++ server/src/main/java/com/ssscloud/auction/server/networking/MessageHandler.java
@@ -3,43 +3,73 @@ package com.ssscloud.auction.server.networking;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.ssscloud.auction.common.dto.response.BidDTO;
+import com.ssscloud.auction.common.dto.ClientMessage;
+import com.ssscloud.auction.common.dto.response.UserDTO;
+import com.ssscloud.auction.common.util.JsonUtils;
+import com.ssscloud.auction.server.controller.AuctionController;
+import com.ssscloud.auction.server.controller.BidController;
+import com.ssscloud.auction.server.controller.UserController;
 
 import netscape.javascript.JSObject;
 
 public class MessageHandler {
     private  Gson gson = new Gson();
-    private BidController bidController = new BidController();
-    private UserController userController = new UserController();
+    private BidController bidController;
+    private UserController userController;
+    private AuctionController auctionController;
 
-    public String proccessRequest(String jsonFromClient){
+    public MessageHandler(UserController userController, 
+                          AuctionController auctionController, 
+                          BidController bidController) {
+        this.userController = userController;
+        this.auctionController = auctionController;
+        this.bidController = bidController;
+    }
+
+    public void handleMessage(String clientId, String jsonMessage) {
         try {
-            JsonObject requestClient = JsonParser.parseString(jsonFromClient).getAsJsonObject();
+            //Chuyển JSON thành ClientMessage object
+            ClientMessage msg = JsonUtils.fromJson(jsonMessage, ClientMessage.class);
 
-            String action = requestClient.get("action").getAsString();
-            String payload = requestClient.getAsJsonObject("payload");
+            if (msg == null || msg.getAction() == null) {
+                System.err.println("Message không hợp lệ từ client " + clientId);
+                return;
+            }
 
+            String action = msg.getAction().toUpperCase().trim();
+
+            //Dựa vào action để gọi Controller phù hợp
             switch (action) {
-                case "PLACE_BID":
-                    BidDTO bidDTO = gson.tojson(payload, BidDTO.class);
-                    return bidController.handle(bidDTO);
                 case "LOGIN":
-                    UserDTO userDTO = gson.tojson(payload, UserDTO.class);
-                    return userController.handle(userDTO);
-                case "GET_AUTION_LIST":
-                    return bidController.getAuctionList();
+                    userController.login(clientId, msg.getData());
+                    break;
+
+                case "REGISTER":
+                    userController.register(clientId, msg.getData());
+                    break;
+
+                case "CREATE_AUCTION":
+                    auctionController.createAuction(clientId, msg.getData());
+                    break;
+
+                case "PLACE_BID":
+                    bidController.placeBid(clientId, msg.getData());
+                    break;
+
+                case "AUTO_BID":
+                    bidController.registerAutoBid(clientId, msg.getData());
+                    break;
+
                 default:
-                    return createErrorReponse("Hành động không hợp lệ");
-            }   
+                    System.err.println("Action không được hỗ trợ: " + action + " từ client " + clientId);
+            }
+
         } catch (Exception e) {
+            System.err.println("Lỗi xử lý message từ client " + clientId + ": " + e.getMessage());
             e.printStackTrace();
-            return createErrorReponse("Lỗi cú pháp Json hoặc lỗi hệ thống");
         }
     }
 
-    private createErrorReponse(String errorMessage){
-            JsonObject errorReponse = new JSObject();
-            errorReponse.addProperty("status", "Error");
-            errorMessage.addProperty("message", errorMessage);
-            return errorMessage.toString();
-    }
+
 }
