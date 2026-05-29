84fa505d0fbdcb87ea0dadce516eeb70d24ecb64
Hoàng Hà Anh <intotheunknownohhh@gmail.com>
2026-05-29 20:07:20 +0700

    fix: sửa background ui

--- client/src/main/java/com/ssscloud/auction/client/controller/bidder/BidderDashboardController.java
+++ client/src/main/java/com/ssscloud/auction/client/controller/bidder/BidderDashboardController.java
@@ -45,10 +45,10 @@ public class BidderDashboardController implements MessageListener {
 
     private int currentBannerIndex = 0;
     private final String[] bannerUrls = {
-        "https://images.unsplash.com/photo-1593640408182-31c70c8268f5?w=1920&h=400&fit=crop", 
-        "https://images.unsplash.com/photo-1618366712010-f4ae9c647dcb?w=1920&h=400&fit=crop", 
+        "https://i.pinimg.com/736x/80/d3/ce/80d3ce2d95bc7a4b483ad61acd417e40.jpg", 
+        "https://i.pinimg.com/1200x/d4/02/47/d4024736403321036254b43140c7cc45.jpg", 
         "https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?w=1920&h=400&fit=crop", 
-        "https://images.unsplash.com/photo-1583121274602-3e2820c69888?w=1920&h=400&fit=crop"  
+        "https://i.pinimg.com/1200x/18/0f/37/180f37f9d241985c23cee550893ea4ec.jpg"  
     };
 
     private final SocketDispatcher dispatcher = SocketDispatcher.getInstance();
